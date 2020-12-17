e6521a3517ebcc938deb16078a8d5df733cb200d
yizhiyan1992 <u1207314@utah.edu>
2020-12-16 23:45:29 -0700

    revision

--- task02/js/ajax-utils.js
+++ task02/js/ajax-utils.js
@@ -1,9 +1,9 @@
 
-(function(global) {
+(function (global) {
 var ajax={};
 
 ajax.sendGetRequest=
-	function(url,handler) {
+	function(url,handler,isjson) {
 		function create_xhr() {
 			if (global.XMLHttpRequest) {
 				return (new XMLHttpRequest());
@@ -12,14 +12,22 @@ ajax.sendGetRequest=
 				return (new ActiveXObject("Microsoft.XMLHTTP"));
 			}
 		}
-		
 		var xhr=create_xhr()  //create a new XHR object
 		xhr.open("GET",url,true) ;//set the open property
 		xhr.send(null);//set the send property
 		xhr.onreadystatechange=
 			function (){
+				//console.log(xhr.status+' '+xhr.readyState)
 				if (xhr.status==200 && xhr.readyState==4) {
-					handler(xhr);
+					if (isjson==undefined) {
+						isjson=true;
+					}
+					if (isjson==true) {
+						handler(JSON.parse(xhr.responseText));
+					}
+					else {
+						handler(xhr.responseText);
+					}
 				}
 			}
 	}
--- task02/js/script.js
+++ task02/js/script.js
@@ -18,9 +18,66 @@ $(function() {
 
 
 	$ajax.sendGetRequest("snippet/snippet-carousel.html",
-		function (xhr) {
-			var content=xhr.responseText
+		function (text) {
+			var content=text
 			global.document.querySelector("#carouselScroll")
 				.innerHTML=content
-	})
+	},false)
 })(window)
+
+	// for team page
+	var insertProperty=function (string,propName,propValue) {
+		var propToReplace="{{"+propName+'}}';
+		string=string.replace(new RegExp(propToReplace,"g"),propValue);
+		return string
+	}
+
+	var teamSnippet="snippet/snippet-team.html";
+	$ajax.sendGetRequest(teamSnippet,
+		function (text) {
+			var teamContent=text;
+			$ajax.sendGetRequest("snippet/team.json",
+				function (text) {
+					var	teamInfo=text;
+					var final_html=""
+					for (var i=0; i<teamInfo.length; i++) {
+						var temp_html=teamContent;
+						var name=teamInfo[i].name;
+						var email=teamInfo[i].Email;
+						var intro=teamInfo[i].introduction;
+						var photo=teamInfo[i].PhotoUrl;
+						temp_html=insertProperty(temp_html,"name",name);
+						temp_html=insertProperty(temp_html,"email",email);
+						temp_html=insertProperty(temp_html,"intro",intro);
+						temp_html=insertProperty(temp_html,"PhotoUrl",photo);
+						final_html+=temp_html+"<hr class='featurette-divider team'>"
+						}
+					window.document.querySelector(".team").innerHTML=final_html
+				
+				})
+
+		},false)
+	// for publication page
+	var publications="snippet/snippet-publications.html";
+	$ajax.sendGetRequest(publications,
+		function(text) {
+			var publicationSnippet=text;
+			$ajax.sendGetRequest("snippet/publications.json",
+				function (text) {
+					var pubInfo=text;
+					var final_list="";
+					for (var i=0;i<pubInfo.length;i++) {
+						var tempPub=publicationSnippet;
+						var paperName=pubInfo[i].name;
+						var link=pubInfo[i].link;
+						tempPub=insertProperty(tempPub,"publication",paperName)
+						tempPub=insertProperty(tempPub,"link",link)
+						final_list+=tempPub}
+						
+					window.document.querySelector("#publications-list").innerHTML=final_list;
+				
+				},true)
+		},false)
+	
+
+
